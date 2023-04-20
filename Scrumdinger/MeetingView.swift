ce2e385df208f6c35bc5b84ace7faf8b92e928d8
khk811 <hyunkyungkim811@gmail.com>
2023-04-20 19:57:37 +0900

    ADD: accessibility tag 추가

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -22,6 +22,9 @@ struct MeetingView: View {
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("time remaining")
+            .accessibilityValue("10 minutes")
             Circle().strokeBorder(lineWidth: 24)
             HStack {
                 Text("Speaker 1 of 3")
@@ -29,6 +32,7 @@ struct MeetingView: View {
                 Button (action: {}) {
                     Image(systemName: "forward.fill")
                 }
+                .accessibilityLabel("Next speaker")
             }
         }
         .padding()
