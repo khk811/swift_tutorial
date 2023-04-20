e34ea5d65236ec1727fe269dcb7ee12c99d65b26
khk811 <hyunkyungkim811@gmail.com>
2023-04-20 20:58:05 +0900

    ADD: cardview 실습 (color asset추가, enum, struct 추가)

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,34 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by 김현경 on 2023/04/20.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack (alignment: .leading){
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: /*@START_MENU_TOKEN@*/"clock"/*@END_MENU_TOKEN@*/)
+            }
+        }
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
+
