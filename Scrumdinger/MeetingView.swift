fdfc8d39c4f86f996b827cebd35de8cf2a9a409c
khk811 <hyunkyungkim811@gmail.com>
2023-04-20 17:24:49 +0900

    ADD: Basic Compose views 작성

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by 김현경 on 2023/04/20.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
