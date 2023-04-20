aa2927dd6371f9e37d838f47459610566c311821
khk811 <hyunkyungkim811@gmail.com>
2023-04-20 17:42:57 +0900

    ADD: spacer, circle 및 버튼 추가

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -12,17 +12,26 @@ struct MeetingView: View {
         VStack {
             ProgressView(value: 5, total: 15)
             HStack {
-                VStack {
-                    Text("Seconds Elapsed")
+                VStack (alignment: .leading) {
+                    Text("Seconds Elapsed").font(.caption)
                     Label("300", systemImage: "hourglass.tophalf.fill")
                 }
                 Spacer()
-                VStack {
-                    Text("Seconds Remaining")
+                VStack (alignment: .trailing) {
+                    Text("Seconds Remaining").font(.caption)
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
+            Circle().strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button (action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
         }
+        .padding()
     }
 }
 
