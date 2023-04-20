5254e83c99ed4c46e5f820a89211fbee40806e67
khk811 <hyunkyungkim811@gmail.com>
2023-04-20 16:50:39 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 김현경 on 2023/04/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
