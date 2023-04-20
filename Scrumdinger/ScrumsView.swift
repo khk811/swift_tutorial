aa93f43ba3c6a70fd83643cf2bae70bc9eb3b873
khk811 <hyunkyungkim811@gmail.com>
2023-04-20 21:41:57 +0900

    ADD: 샘플 데이터를 list 형태로 표현함

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,24 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by 김현경 on 2023/04/20.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrumArray: [DailyScrum]
+    var body: some View {
+        List(scrumArray, id: \.title) { scrumItem in
+            CardView(scrum: scrumItem)
+                .listRowBackground(scrumItem.theme.mainColor)
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrumArray: DailyScrum.sampleData)
+    }
+}
